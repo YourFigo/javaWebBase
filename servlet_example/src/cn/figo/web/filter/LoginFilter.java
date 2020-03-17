a53422420d10a2552c31a92fcff4c04e0afbbef2
Figo <37700763+YourFigo@users.noreply.github.com>
2020-03-17 11:54:39 +0800

    修改 servlet_example模块中的 LoginFilter类

--- servlet_example/src/cn/figo/web/filter/LoginFilter.java
+++ servlet_example/src/cn/figo/web/filter/LoginFilter.java
@@ -11,9 +11,11 @@ import java.io.IOException;
  */
 @WebFilter("/*")
 public class LoginFilter implements Filter {
+    @Override
     public void destroy() {
     }
 
+    @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
         //0.强制转换
         HttpServletRequest request = (HttpServletRequest) req;
@@ -41,6 +43,7 @@ public class LoginFilter implements Filter {
 
     }
 
+    @Override
     public void init(FilterConfig config) throws ServletException {
 
     }
