712ca430c495d492c0bf15f04ed53cea3daf8462
Figo <37700763+YourFigo@users.noreply.github.com>
2020-03-27 15:33:56 +0800

    学习 vue-router

---
+++ es6_vue/src/js/login.js
@@ -0,0 +1,12 @@
+const loginForm = {
+    // template 中只能有一个根标签，不能有其他平行标签
+    template:`
+    <div>
+        <h2>登录页</h2> 
+        用户名：<input type="text"><br/>
+        密&emsp;码：<input type="password"><br/>
+        <input type="button" value="登录">
+    </div>
+    `
+
+}
---
+++ es6_vue/src/js/register.js
@@ -0,0 +1,12 @@
+const registerForm = {
+    // template 中只能有一个根标签，不能有其他平行标签
+    template:`
+    <div>
+        <h2>注册页</h2> 
+        用&ensp;户&ensp;名：<input type="text"><br/>
+        密&emsp;&emsp;码：<input type="password"><br/>
+        确认密码：<input type="password"><br/>
+        <input type="button" value="注册">
+    </div>
+    `
+}
